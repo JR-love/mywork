24e1b78687f58df41bbf568c49832a4c38adbc84
JR <1459323294@qq.com>
2020-06-29 19:43:05 +0800

    我的项目

---
+++ src/main/java/com/jr/giteeDemo/GiteedemoApplication.java
@@ -0,0 +1,13 @@
+package com.jr.giteeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiteedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiteedemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jr/giteeDemo/GiteedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.giteeDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiteedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
